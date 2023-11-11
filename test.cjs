11e01ce9b69f180dfd5621e94976231a0f84b470
Googlefan <private@googlefan.net>
2023-11-11 17:43:18 +0900

    fix: test.cjs

--- test.cjs
+++ test.cjs
@@ -1,6 +1,6 @@
-const { parse, colorlize, simpleHtml, DARK } = require(".");
+const { autoparse, colorlize, simpleHtml, DARK } = require(".");
 const { readFileSync } = require("fs");
-const js = parse("test.cjs", readFileSync("test.cjs", "utf-8"))[1];
+const js = autoparse("test.cjs", readFileSync("test.cjs", "utf-8"))[1];
 const [jsc, bg] = colorlize(js, DARK);
 const jsm = simpleHtml(jsc, bg);
 
